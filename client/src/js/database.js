9a3651a691801fae7c15ba5b487b25260b306096
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-08-16 18:01:06 -0400

    get req in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// add some content to db
+  export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// get all the content from the database
+export const getDb = async () =>  {
+  console.log('GET from the database');
+
+  // connect to the database and specify version
+  const contactDb = await openDB('contact', 1);
+
+  // new transaction and specify database and privileges.
+  const tx = contactDb.transaction('contact', 'readonly');
+
+  // open obj store
+  const store = tx.objectStore('contact');
+
+  // get all data in the db
+  const request = store.getAll();
+
+  // confirm req
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}; console.error('getDb not implemented');
 
 initdb();
